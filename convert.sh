41d8675303b4465a6f6bbf49f755c1962c2d6379
Mark Hoekveen <mhoekveen@gmail.com>
2020-07-11 23:15:23 +0200

    improve initial loadtimes Make the shitty jpeg actually shitty to significantly decrease filesize

--- convert.sh
+++ convert.sh
@@ -6,4 +6,4 @@ file="$HOME/.cache/wallpaper"
 convert $file img/bg/high.jpg
 convert $file -quality 50 img/bg/med.jpg
 convert $file -quality 25 img/bg/low.jpg
-convert $file -blur 0x8 -quality 20 img/bg/shit.jpg
+convert $file -blur 0x8 -resize 25% -quality 10 img/bg/shit.jpg
